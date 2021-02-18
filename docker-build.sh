84b201e44e19b341da87f389dba25b19a99e6c46
estherlpgras11 <esther.lpgras@gmail.com>
2021-02-18 17:36:08 +0100

    Add k8s manifests: deployments, services, secret, pvc and cm

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t eu.gcr.io/kainight/counter-flask:v1.0 .
-docker push eu.gcr.io/kainight/counter-flask:v1.0
+docker build -t eu.gcr.io/kainight/flask:v1.1 .
+docker push eu.gcr.io/kainight/flask:v1.1
